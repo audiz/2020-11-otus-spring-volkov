92f28056a3bb0f6e0ceb5650f2a28e604a3c438a
audiz <artem1911@bk.ru>
2020-12-15 22:53:47 +0500

    work5 tests

--- work5/src/test/java/ru/otus/work5/dao/TestQuestionDao.java
+++ work5/src/test/java/ru/otus/work5/dao/TestQuestionDao.java
@@ -2,32 +2,36 @@ package ru.otus.work5.dao;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.shell.jline.InteractiveShellApplicationRunner;
+import org.springframework.shell.jline.ScriptShellApplicationRunner;
 import ru.otus.work5.domain.Question;
 import ru.otus.work5.service.DataReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(properties = {
+        InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
+        ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
+})
 @DisplayName("Класс QuestionDaoImpl")
 public class TestQuestionDao {
+
+    @Autowired
+    private QuestionDao dao;
+
+    @MockBean
+    private DataReader dataReader;
 
     @DisplayName("корректно парсит строку с вариантами")
     @Test
     void shouldReadLine() {
 
-        DataReader fileReader = new DataReader() {
-            @Override
-            public String readLine() {
-                return "Which of the following is not a Java features?;Dynamic;1;Architecture Neutral;1;Use of pointers;0;Object-oriented;0";
-            }
-
-            @Override
-            public void restart() {
-
-            }
-        };
-        QuestionDaoImpl dao = new QuestionDaoImpl(fileReader, ";");
+        Mockito.when(dataReader.readLine()).thenReturn("Which of the following is not a Java features?;Dynamic;1;Architecture Neutral;1;Use of pointers;0;Object-oriented;0");
         Question question = dao.getNextQuestion();
-
         assertNotNull(question);
         assertEquals(4,  question.getAnswers().size());
         assertTrue(question.getAnswers().get(0).isCorrect());
@@ -37,19 +41,7 @@ public class TestQuestionDao {
     @DisplayName("корректно парсит строку с пользовательским вводом")
     @Test
     void shouldReadUserLine() {
-
-        DataReader fileReader = new DataReader() {
-            @Override
-            public String readLine() {
-                return "What is the return type of the hashCode() method in the Object class?;#USER_INPUT#;int";
-            }
-
-            @Override
-            public void restart() {
-
-            }
-        };
-        QuestionDaoImpl dao = new QuestionDaoImpl(fileReader, ";");
+        Mockito.when(dataReader.readLine()).thenReturn("What is the return type of the hashCode() method in the Object class?;#USER_INPUT#;int");
         Question question = dao.getNextQuestion();
 
         assertNotNull(question);
