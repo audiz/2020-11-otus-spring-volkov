7ea91ae4f9085ab5977f7546de7141d40be06976
audiz <artem1911@bk.ru>
2021-03-03 22:50:07 +0500

    work23

---
+++ work23/src/main/java/ru/otus/work23/service/SequenceGeneratorService.java
@@ -0,0 +1,27 @@
+package ru.otus.work23.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+import ru.otus.work23.domain.DbSequence;
+
+import java.util.Objects;
+
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+
+@Service
+@RequiredArgsConstructor
+public class SequenceGeneratorService {
+
+    private final MongoOperations mongoOperations;
+
+    public int getSequenceNumber(String sequenceName) {
+        Query query = new Query(Criteria.where("id").is(sequenceName));
+        Update update = new Update().inc("seq", 1);
+        DbSequence counter = mongoOperations.findAndModify(query, update, options().returnNew(true).upsert(true), DbSequence.class);
+        return !Objects.isNull(counter) ? counter.getSeq() : 1;
+    }
+}
---
+++ work23/src/test/java/ru/otus/work23/rest/ServiceControllerTest.java
@@ -0,0 +1,52 @@
+package ru.otus.work23.rest;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTypeExcludeFilter;
+import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// From @DataMongoTest
+@ExtendWith({SpringExtension.class})
+@OverrideAutoConfiguration(
+        enabled = false
+)
+@TypeExcludeFilters({DataMongoTypeExcludeFilter.class})
+@AutoConfigureCache
+@AutoConfigureDataMongo
+@ImportAutoConfiguration
+// From @DataMongoTest
+
+@EnableConfigurationProperties
+@ComponentScan({"ru.otus.work23.service", "ru.otus.work23.domain", "ru.otus.work23.changelog"})
+@RunWith(SpringRunner.class)
+@WebMvcTest(ServiceController.class)
+public class ServiceControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testAuthinfo() throws Exception {
+        mockMvc.perform(get("/api/authinfo")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testSummary() throws Exception {
+        mockMvc.perform(get("/api/summary")).andExpect(status().isOk());
+    }
+}
